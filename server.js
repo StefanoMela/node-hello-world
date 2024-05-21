926c04818c2cde311b6559152e21e0103417e1bc
StefanoMela <stefano.mela25@gmail.com>
2024-05-21 16:18:41 +0200

    feat: hello world + bonus

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
+
+const tips = process.env.TIPS.split(',');
+
+function getRandom() {
+    let randomIndex = Math.floor(Math.random() * tips.length);
+    return tips[randomIndex];
+}
+
+http.createServer(function (req, res) {
+    if (req.url === '/favicon.ico') {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    res.writeHead(200, {
+        "Content-type": "text/html"
+    });
+    res.end(`<h1>Try: ${getRandom()}</h1>`);
+})
+    .listen(port, host, () => {
+        console.log(`Server avviato su http://${host}:${port}`);
+    })
